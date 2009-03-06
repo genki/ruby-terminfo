1cb427d8e08abdef68cd2bc9b88044c6852e3951
Tanaka Akira <akr@fsij.org>
2009-03-06 07:41:48 +0000

    update year.
    git-svn-id: svn://svn.a-k-r.org/akr/ruby-terminfo/trunk@992 0ecacc03-ac1b-0410-941d-e442c79421a6

--- terminfo.c
+++ terminfo.c
@@ -1,6 +1,6 @@
 /* terminfo.c - Ruby binding for terminfo library.
 
-Copyright (C) 2007 Tanaka Akira. All rights reserved.
+Copyright (C) 2007, 2008 Tanaka Akira. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
